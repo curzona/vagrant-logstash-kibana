9aa0fb2cea7b7091acddb82eb091b1c8a39608f1
Aron Curzon <curzona@gmail.com>
2014-07-12 00:09:19 -0700

    Move service startup into the provision script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,17 @@ apt-get -y install vim curl zip
 apt-get -y install openjdk-7-jre-headless
 
 wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.0.tar.gz
-tar zxvf logstash-1.4.0.tar.gz
-mv logstash-1.4.0 /usr/share/logstash
+tar -zxvf logstash-1.4.0.tar.gz
+mv logstash-1.4.0 /opt/logstash
 
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.0.deb
 sudo dpkg -i elasticsearch-1.1.0.deb
 sudo service elasticsearch start
 rm elasticsearch-1.1.0.deb
+
+cp /vagrant/logstash-shipper.conf /etc/logstash-shipper.conf
+cp /vagrant/logstash-shipper.upstart /etc/init/logstash-shipper.conf
+service logstash-shipper start
+
+cp /vagrant/logstash-web.upstart /etc/init/logstash-web.conf
+service logstash-web start
--- install-service.sh
+++
@@ -1,6 +0,0 @@
-cp logstash-shipper.conf /etc/logstash-shipper.conf
-cp logstash-shipper.upstart /etc/init/logstash-shipper.conf
-service logstash-shipper start
-
-cp logstash-web.upstart /etc/init/logstash-web.conf
-service logstash-web start
